d6a00cb4e5a4fc95fd50509c111852b0abb7db69
gdangelo <dangelo.gregory@gmail.com>
2015-05-20 21:38:17 +0200

    Handle filtering and sorting courses list

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -1,3 +1,9 @@
 angular.module('app').controller('mvCourseListCtrl', function($scope, mvCourse){
 	$scope.courses = mvCourse.query();
+
+	$scope.sortOptions = [
+		{value:"title", text:"Sort by title"},
+		{value:"published", text:"Sort by published date"}
+	];
+	$scope.sortOrder = $scope.sortOptions[0].value;
 });
